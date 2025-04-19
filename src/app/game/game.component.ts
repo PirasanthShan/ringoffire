6bba06758a31a5c607ab28d9e83eebc6c61dade4
Pirasanth Shanmuganathan <pirasanth@live.de>
2025-04-19 12:35:49 +0200

    Firebase Project

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+ pickCardAnimation = false; 
+ currentCard: string | undefined = '';
+ game: Game | undefined;
+
+ constructor( public dialog: MatDialog){}
+
+ngOnInit(): void {
+  this.newGame();
+}
+
+newGame(){
+  this.game = new Game();
+  console.log(this.game);
+}
+  
+  takeCard() {
+    if(!this.pickCardAnimation) {
+    this.currentCard = this.game!.stack.pop();
+    console.log(this.currentCard)
+    this.pickCardAnimation = true;
+    
+    console.log('New card:', this.currentCard);
+    console.log('Game is', this.game)
+    
+    this.game!.currentPlayer!++;
+    this.game!.currentPlayer = this.game!.currentPlayer % this.game!.players.length;
+    setTimeout(() => {
+      this.game?.playedCard.push(this.currentCard!);
+      this.pickCardAnimation = false;
+    }, 1000);
+  }
+ }
+
+ openDialog(): void {
+  const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+   
+   dialogRef.afterClosed().subscribe((name: string) => {
+    this.game?.players.push(name);
+  });
+}
+
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBynpR_QDku8QSol9HlST2GCL-rIqEZOBM",
+    authDomain: "ring-of-fire-2c2f3.firebaseapp.com",
+    projectId: "ring-of-fire-2c2f3",
+    storageBucket: "ring-of-fire-2c2f3.firebasestorage.app",
+    messagingSenderId: "520865676723",
+    appId: "1:520865676723:web:041e9e7220a31045555fc6",
+    measurementId: "G-6EPLNDXNWN"
+  }
+};
